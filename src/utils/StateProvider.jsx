a1f8305613ae39d53860d915f6246dfa3a517c30
WOOWOOYONG <woowooyong@gmail.com>
2022-09-20 21:28:17 +0800

    9/20 [building...]

---
+++ src/utils/StateProvider.jsx
@@ -0,0 +1,11 @@
+import { createContext, useContext, useReducer } from "react";
+
+export const StateContext = createContext();
+
+export const StateProvider = ({ children, initailState, reducer }) => (
+  <StateContext.Provider value={useReducer(reducer, initailState)}>
+    {children}
+  </StateContext.Provider>
+);
+
+export const useStateProvider = () => useContext(StateContext);
